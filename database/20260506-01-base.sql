1b37be3ee73c9f722e0c273922cae37bad115b1d
DirkTomefy <DirkTomefy@gmail.com>
2026-05-07 08:38:28 +0300

    [fix] : sql fixed

--- database/20260506-01-base.sql
+++ database/20260506-01-base.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE diet ;
 
+use diet;
+
 -- ============================================================
 --  BASE DE DONNÉES : APPLICATION RÉGIME ALIMENTAIRE
 -- ============================================================
